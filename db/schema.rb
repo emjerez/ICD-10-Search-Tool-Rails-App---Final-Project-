4b6931f62e3022df814e16ff8a2be872f5f8d87e
Valeria Protsan <valeriyapro@gmail.com>
2018-04-07 17:53:31 -0400

    added pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180407191245) do
+ActiveRecord::Schema.define(version: 20180407214951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,12 @@ ActiveRecord::Schema.define(version: 20180407191245) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["code_id"], name: "index_inclusion_terms_on_code_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "index"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "recents", force: :cascade do |t|
