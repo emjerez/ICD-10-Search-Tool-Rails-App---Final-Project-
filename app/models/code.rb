feeaac80fab398bf5c452e1a42af5e0c881782c7
Ethel Jerez <ethelmj@gmail.com>
2018-04-08 15:00:55 -0400

    Added ElasticSearch functionality

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,4 +1,10 @@
+require 'elasticsearch/model'
 class Code < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+  # index_name Rails.application.class.parent_name.underscore
+  # document_type self.name.downcase
+
   serialize :includes
   serialize :inclusionTerm
   serialize :excludes1
@@ -9,6 +15,21 @@ class Code < ApplicationRecord
   # belongs_to :parent, inverse_of: :children
   # belongs_to :parent, :class_name => "Code", :join_table => :relationships, foreign_key: :child_id
   #for top level parent create excliusion because it wont have a parent
+
+
+  # def self.search(query)
+  #   __elasticsearch__.search(
+  #     {
+  #       size: 1000,
+  #       query: {
+  #         multi_match: {
+  #           query: query,
+  #           fields: ['code_id^10', 'desc']
+  #         }
+  #       }
+  #     }
+  #   )
+  # end
 
   def children
     Code.where(parent_id: self.code_id)
